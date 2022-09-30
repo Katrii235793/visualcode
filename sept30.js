b735d337eb708188c00915ce643a9f1e2bf5645e
Katrii235793 <235793@ust-global.com>
2022-09-30 16:55:15 +0530

    sept30

---
+++ sept30.js
@@ -0,0 +1,17 @@
+
+           document.write("Hello World!")
+        let name = 'ust';
+        let num=1;
+        let addr=null;
+typeof name
+
+
+        let person={
+            name:'Katrii',
+             age:22,
+
+        };
+        person.name='Katrii';
+        console.log(person.name);
+
+    
